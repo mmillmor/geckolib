f0b3a5c56a87692d892e15c0703df60ad3ccfb9e
Martin Millmore <martin@milliesoft.co.uk>
2022-01-03 21:34:08 +0000

    moved files for library build

--- tests/simulator.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""
-    Spa simulator program for geckolib: Pretends to be a spa on the network,
-    used by integration tests to prevent regression
-"""
-
-from context import GeckoSimulator
-
-GeckoSimulator.run(["logfile simulator.log", "start"])
